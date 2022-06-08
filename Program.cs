6371dddfbe89e3be2e1c5c4ca922cf2cd35535ec
Luke Dickinson <lukerobertdickinson@gmail.com>
2022-06-08 20:48:58 +1000

    Added Canadian invoices

--- Program.cs
+++ Program.cs
@@ -13,11 +13,15 @@ namespace XeroTaxCalculator
             {
                 new Invoice(199.99M, "AU"),
                 new Invoice(2.49M, "NZ"),
-                new Invoice(110.00M, "AU")
+                new Invoice(110.00M, "AU"),
+                new Invoice(10.00M, "CA"),
+                new Invoice(6.00M, "NZ"),
+                new Invoice(54.00M, "AU")
             };
 
             resultDict.Add("AU", calculator.CalculateGST(orders, "AU"));
             resultDict.Add("NZ", calculator.CalculateGST(orders, "NZ"));
+            resultDict.Add("CA", calculator.CalculateGST(orders, "CA"));
         }
     }
 }
